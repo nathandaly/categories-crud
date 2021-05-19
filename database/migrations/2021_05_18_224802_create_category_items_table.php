774f63165e45d1493dbd3138b3db5688c4988ba9
Nathan Daly <nathandalypretoria@hotmail.co.uk>
2021-05-19 11:15:26 +0100

    Adding migrations for categories and items with a many to many relation.

---
+++ database/migrations/2021_05_18_224802_create_category_items_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoryItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('category_id');
+            $table->unsignedBigInteger('item_id');
+
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->foreign('item_id')->references('id')->on('items');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('category_items');
+    }
+}
